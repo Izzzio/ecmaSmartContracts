97459a60ca1c9b0ac98925c4bbc230f8e6077663
Andrey Nedobylsky <admin@twister-vl.ru>
2018-09-14 00:57:07 +0300

    Create WalletRegister.js

---
+++ WalletRegister.js
@@ -0,0 +1,29 @@
+/**
+ * Wallet registrator and checker
+ */
+class WalletRegister extends Contract {
+    init() {
+        this.wallets = new BlockchainObject('wallets');
+    }
+
+    /**
+     * Register new wallet and public key
+     */
+    registerWallet(address, publicKey) {
+        assert.false(this.wallets[address], 'Wallet already registered');
+        this.wallets[address] = {address: address, key: publicKey};
+    }
+
+    /**
+     * Check is wallet registered
+     */
+    checkWallet(address) {
+        const wallet = this.wallets[address];
+        if(!wallet) {
+            return false;
+        }
+        return wallet;
+    }
+}
+
+global.registerContract(WalletRegister);
